f93a8647ead7f9176299a65722f338b482474289
WAY29 <1162410187@qq.com>
2022-05-26 11:54:33 +0800

    update tmux.sh

--- tmux.sh
+++ tmux.sh
@@ -5,10 +5,11 @@ tmux has-session -t $session 2>/dev/null
 if [ $? != 0 ]
 then
 	tmux new-session -s $session -d
-    tmux split-window -h -t $session:1.1
-	tmux split-window -v -t $session:1.1
+	tmux split-window -h -t $session:1.1
 	tmux new-window -n zsh
+	tmux split-window -h -t $session:2.1
     tmux new-window -n zsh
+	tmux split-window -h -t $session:3.1
 fi
 tmux select-window -t $session:1
 tmux select-pane -t $session:1.1 2>/dev/null
